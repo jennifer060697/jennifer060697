45d8534194e9d9bf06ff9be0da0094d6d5c40b93
YouLim Oh <63313306+jennifer060697@users.noreply.github.com>
2023-06-02 14:15:31 +0900

    Update update_blogPost.py

--- update_blogPost.py
+++ update_blogPost.py
@@ -15,6 +15,7 @@ for idx, entrie in enumerate(rss_feed['entries']):
   latest_posts += f" - [{feed_date.tm_mon}/{feed_date.tm_mday} - {entrie['title']}]({entrie['link']})\n"
 
 preREADME = """
+
 <div align="center">
 <img src="https://capsule-render.vercel.app/api?type=waving&color=timeGradient&height=300&section=header&text=JAMONG%205&fontSize=90" />
 </div>
@@ -25,16 +26,16 @@ preREADME = """
 <h2 align="center">☾⋆⁺LINK⁺⋆☾</h2>
 <div align="center">
   <a href="https://jamong-5.tistory.com/"><img src="https://img.shields.io/badge/TechBlog-09B3AF?style=flat-square&logo=Tistory&logoColor=white&link=https://jamong-5.tistory.com/"/></a>
-  <a href="https://www.kaggle.com/jamong5"><img src="https://img.shields.io/badge/Kaggle-20BEFF?style=flat-square&logo=Kaggle&logoColor=white&link=https://www.kaggle.com/jamong5"/></a>
+<!--   <a href="https://www.kaggle.com/jamong5"><img src="https://img.shields.io/badge/Kaggle-20BEFF?style=flat-square&logo=Kaggle&logoColor=white&link=https://www.kaggle.com/jamong5"/></a> -->
   <a href="mailto:oennifer060697@gmail.com"><img src="https://img.shields.io/badge/Email-FF4785?style=flat-square&logo=Gmail&logoColor=white&link=mailto:oennifer060697@gmail.com"/></a>
 </div>
 
-<h2 align="center">☾⋆⁺Available⁺⋆☾</h2>
+<!-- <h2 align="center">☾⋆⁺Available⁺⋆☾</h2>
 <div align="center">
   <img src="https://img.shields.io/badge/Python-00B1E7?logo=Python&logoColor=white"/>
   <img src="https://img.shields.io/badge/C++-00599C?logo=C%2B%2B&logoColor=white"/>
   <img src="https://img.shields.io/badge/C-000000?logo=C&logoColor=white"/>
-</div>
+</div> -->
 
 <br><br>
 
@@ -54,6 +55,7 @@ preREADME = """
 
 <br><br>
 #### My Latest Posts
+
 """
 
 resultREADME = f"{preREADME}{latest_posts}"
