0fd6720c768b96df197bd410df54e1e21c07d4bb
YouLim Oh <63313306+jennifer060697@users.noreply.github.com>
2023-06-02 14:49:51 +0900

    Update update_blogPost.py

--- update_blogPost.py
+++ update_blogPost.py
@@ -47,6 +47,8 @@ preREADME = """
   <img src = "https://github-readme-stats.vercel.app/api/top-langs/?username=jennifer060697&layout=compact">
 </div>
 
+<br><br>
+
 #### 🌱 I’m currently learning ...
 - Pytorch
 - MMDectection, YOLO
