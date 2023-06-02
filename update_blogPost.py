d7e7a69d76e3f458505a1eaf6d23080e6bf2e5fe
YouLim Oh <63313306+jennifer060697@users.noreply.github.com>
2023-06-02 16:44:38 +0900

    Update update_blogPost.py

--- update_blogPost.py
+++ update_blogPost.py
@@ -25,7 +25,7 @@ preREADME = """
 
 <h2 align="center">☾⋆⁺LINK⁺⋆☾</h2>
 <div align="center">
-  <a href="https://jamong-5.tistory.com/"><img src="https://img.shields.io/badge/TechBlog-09B3AF?style=flat-square&logo=Tistory&logoColor=white&link=https://jamong-5.tistory.com/"/></a>
+  <a href="https://jamong-5.tistory.com/"><img src="https://img.shields.io/badge/DailyBlog-09B3AF?style=flat-square&logo=Tistory&logoColor=white&link=https://jamong-5.tistory.com/"/></a>
 <!--   <a href="https://www.kaggle.com/jamong5"><img src="https://img.shields.io/badge/Kaggle-20BEFF?style=flat-square&logo=Kaggle&logoColor=white&link=https://www.kaggle.com/jamong5"/></a> -->
   <a href="mailto:oennifer060697@gmail.com"><img src="https://img.shields.io/badge/Email-FF4785?style=flat-square&logo=Gmail&logoColor=white&link=mailto:oennifer060697@gmail.com"/></a>
 </div>
